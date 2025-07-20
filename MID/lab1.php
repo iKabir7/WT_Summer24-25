e4890f0eeaed57d8cd6dd1505605fda3dae9df54
Md Iqramul Kabir <159653305+iKabir7@users.noreply.github.com>
2025-07-20 10:51:03 +0600

    Add files via upload

---
+++ MID/lab1.php
@@ -0,0 +1,84 @@
+<!DOCTYPE html>
+<html>
+    <title>Aiub Registration Form </title>
+    <body>
+        <center>
+
+    <h1 style = "color : blue "> AIUB </h1>
+    <h2 style = "color : blue "> Registration Form </h2>
+</center>
+
+<table>
+    
+        <h3> Complete the Registration </h3>
+
+        <tr>
+            <td> Full Name : </td>
+            <td> <input type = "name"></td>
+</tr>  
+         <tr>
+            <td> Email : </td>
+            <td> <input type = "email"></td>
+</tr>
+     <tr>
+            <td> Password : </td>
+            <td> <input type = "password"></td>
+</tr>
+
+
+       
+        <tr>
+            <td>
+                Gender  : </td>
+               <td> <input type = "radio" name = "des" > Male 
+                <input type = "radio" name = "des" > Female 
+            </td>
+
+      <tr>
+               <td> Languages Known :  </td>
+                <td><input type = "checkbox" name = "des" > English
+                <input type = "checkbox" name = "des" > Bangla  
+                <input type = "checkbox" name = "des" > Arabic 
+                <input type = "checkbox" name = "des" > Spanish  
+                <input type = "checkbox" name = "des" > German  
+                </td>
+</tr>
+
+              <tr>
+                <td> Country : </td>
+                <td> 
+                   <select name = "Country">
+                    <option value = "">-select-</option>
+                    <option value = "Banladesh">Bangladesh </option>
+                    <option value = "England">England </option>
+                    <option value = "Spain">Spain </option>
+                    <option value = "GErmany">Germany </option>
+                    <option value = "United States">united States </option>
+</td>
+</tr>
+
+             <tr>
+                <td>Date of Birth : </td>
+                <td><input type="date" name="" id=""> </td>
+</tr>
+<tr>
+    <td>Upload Photo :</td>
+    <td><input type= "file "></td>
+</tr>
+
+<tr>
+    <td>Comment : </td>
+<td><input type= "text "></td>
+</tr>
+<tr>
+    <td></td>
+<td><button>SUBMIT</button></td>
+</tr>
+
+
+             
+               
+
+
+</body>
+</html>
