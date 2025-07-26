28adde2d83ed3a05ec70e2263483fff70787e901
iKabir7 <ikh2998@gmail.com>
2025-07-26 23:40:08 +0600

    updated task

--- MID/Lab_Task_2.php
+++ MID/Lab_Task_2.php
@@ -1,1 +1,142 @@
-hello
+
+<!DOCTYPE html>
+<html>
+    <title>ABC Bank</title>
+    <body>
+       <center>
+           <h1 style = "color :blueviolet;"> Bank Management System </h1>
+           <h2 style = "color :blue;"> Your Trusted Financial partner </h2>
+
+       </center>
+    <h3 allign = "right">Customer Registration Form </h3>
+    <table>
+
+
+        <tr>
+            <td> Full Name : </td>
+            <td> <input type = "name"></td>
+</tr>  
+        <tr>
+                <td>Date of Birth : </td>
+                <td><input type="date" name="" id=""> </td>
+</tr>
+          <tr>
+            <td>
+                Gender  : </td>
+               <td> <input type = "radio" name = "des" > Male 
+                <input type = "radio" name = "des" > Female 
+                <input type = "radio" name = "des" > Other
+            </td>
+               
+             <tr>
+                <td> Maritial Status : </td>
+                <td> 
+                   <select name = "maritial status">
+                    <option value = "">-select-</option>
+                    <option value = "Single">Single </option>
+                    <option value = "Married">Married </option>
+                   
+</td>
+</tr>
+
+             <tr>
+                <td> Account Type : </td>
+                <td> 
+                   <select name = "account type">
+                    <option value = "">-select-</option>
+                    <option value = "Savings">Savings </option>
+                    <option value = "Current">current </option>
+                    <option value = "Checking">checking </option>
+                    <option value = "Salary">salary </option>
+                    <option value = "Fixed">fixed </option>
+                   
+</td>
+</tr>
+<tr>
+         <tr>
+            <td> Inital Deposite Amount : </td>
+            <td> <input type = " initial depostie amount"></td>
+</tr>
+     <tr>
+            <td> Mobile Number : </td>
+            <td> <input type = "mobile number"></td>
+</tr>
+   
+
+            <tr>
+            <td> Email Address : </td>
+            <td> <input type = "email address"></td>
+</tr>
+            
+            <tr>
+            <td>  Address : </td>
+            <td> <input type = "address"></td>
+</tr>
+       
+            <tr>
+            <td> Occupation : </td>
+            <td> <input type = "occupation "></td>
+</tr>
+            
+            <tr>
+            <td> National ID (NID) : </td>
+            <td> <input type = "nid"></td>
+</tr> 
+            
+            <tr>
+            <td> Set Password : </td>
+            <td> <input type = "set password"></td>
+</tr>
+     
+             
+            
+
+   <tr>          
+    <td>Upload ID Proof :</td>
+    <td><input type = "file"></td>
+</tr>
+
+ <tr>
+                <td><input type = "checkbox" name = "des" > I agree to the terms and conditions
+                </td>
+</tr>
+
+
+<tr>
+    <td></td>
+<td><button>Register</button>
+    <button>Clear</button></td>
+</tr>
+
+  
+
+</body>
+
+       <style>
+        div {
+            background-color: coral;
+            width: 200px;
+            height: 65px;
+            border: 1px solid black;
+            overflow: scroll;
+        }
+        
+    h1,h2{<color: blue;}
+     body {
+            background-color: skyblue;
+
+     }  
+     
+     table {
+           
+             background-color: white;
+             border: 3cap black;
+             padding: 20px;
+             margin: 20px;
+         
+     }
+</style>
+
+      <div>This is a demo text to show how overflow works  in a small container with a text </div>       
+
+</html>
